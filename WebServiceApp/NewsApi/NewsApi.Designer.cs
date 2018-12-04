cbbc084c33f061eca9c1d58d01846430906cbca0
Andrew Dorre <andrewdorre@gmail.com>
2018-12-04 07:54:17 -0700

    First commit

---
+++ WebServiceApp/NewsApi/NewsApi.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace NewsApi
+{
+    partial class NewsApi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(639, 395);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(295, 414);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // NewsApi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 442);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.textBox1);
+            this.Name = "NewsApi";
+            this.Text = "News - Powered by Newsapi.org";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ WebServiceApp/NewsApi/NewsApi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// https://stackoverflow.com/questions/6620165/how-can-i-parse-json-with-c
+using Newtonsoft.Json.Linq;
+
+namespace NewsApi
+{
+    public partial class NewsApi : Form
+    {
+        private HttpClient client = new HttpClient();
+
+        
+        public NewsApi()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRefresh_Click(object sender, EventArgs e)
+        {
+            string jsonNews = await client.GetStringAsync(new Uri(
+                "https://newsapi.org/v2/top-headlines?country=us&apiKey=5cb35fea7dfd4098abd2498570bdcfb9"));
+            dynamic stuff = JObject.Parse(jsonNews);
+            textBox1.AppendText(stuff);
+        }
+    }
+}
